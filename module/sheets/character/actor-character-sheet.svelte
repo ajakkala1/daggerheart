a71b7f2896f35c46515c4836c87df11548b2b653
echarriguillermo <echarriguillermo@gmail.com>
2024-09-02 23:32:23 -0400

    Continue working on character sheet layout

--- module/sheets/character/actor-character-sheet.svelte
+++ module/sheets/character/actor-character-sheet.svelte
@@ -29,7 +29,6 @@
                     <input class="daggerheart-input" type="text" placeholder="Name" value="{actor.name}" on:input={async e => {
                     // @ts-ignore
                     const updatedName = e.target.value
-                    console.log('updating actor name ', updatedName)
                     update('name', updatedName)
                 }}/>
                 </div>
@@ -52,7 +51,7 @@
     </div>
 
     <!-- Second row contains evasion, armor, armor slots, and the 6 main ability scores -->
-    <div class="flex flex-row">
+    <div class="flex flex-row w-full">
         <Indicator type="evasion" label="EVASION" value="{system.evasionScore.value}"/>
         <Indicator type="armor" label="ARMOR" value="{system.armorScore.value}"/>
         <Resource label="ARMOR SLOTS" value="{system.armor.value}" max="{system.armor.max}" on:update={event => update('system.armor.value', event.detail.value)}/>
@@ -70,53 +69,75 @@
                    on:click={e => roll("1d12[hope]+1d12[fear]+@abilities.knowledge.mod", "Knowledge")}/>
     </div>
 
-
-    <div class="flex flex-row">
-        <div class="resources grid grid-3col">
-            <Resource label="HP" value="{system.hitpoints.value}" max="{system.hitpoints.max}" on:update={event => update('system.hitpoints.value', event.detail.value)}/>
-            <Resource label="STRESS" value="{system.stress.value}" max="{system.stress.max}" on:update={event => update('system.stress.value', event.detail.value)}/>
-
+    <!-- Third row contains HP, Stress, Damage Thresholds, Weapons, Hope, and Experience -->
+    <div class="flex flex-row w-full">
+        <div class="flex flex-col flex-1">
+            <!-- Damage Thresholds -->
+            <div class="flex-row">
+                <span>Minor Damage</span>
+                <span>6</span>
+                <span>Minor Damage</span>
+                <span>12</span>
+                <span>Severe Damage</span>
+            </div>
+            <!-- HP and Stress -->
+            <div class="flex-col">
+                <Resource label="HP" value="{system.hitpoints.value}" max="{system.hitpoints.max}" on:update={event => update('system.hitpoints.value', event.detail.value)}/>
+                <Resource label="STRESS" value="{system.stress.value}" max="{system.stress.max}" on:update={event => update('system.stress.value', event.detail.value)}/>
+            </div>
+        </div>
+        <div class="flex flex-col flex-1">
+            <span>TODO weapons columns</span>
+        </div>
+        <div class="flex flex-col flex-1">
             <Resource label="HOPE" value="{system.hope.value}" max="{system.hope.max}" on:update={event => update('system.hope.value', event.detail.value)}/>
-
-            <div class="resource flex-group-center">
-
-            </div>
-        </div>
-        <div class="abilities flex flex-row">
-            <div class="ability flexrow flex-group-center">
-                <label for="system.abilities.agility.value" class="resource-label rollable flexlarge align-left" data-roll-type="duality" data-roll="1d12[hope]+1d12[fear]+@abilities.agility.mod" data-label="Agility">Agility</label>
-                <input type="text" name="system.abilities.agility.value" value="{system.abilities.agility.value}" data-dtype="Number"/>
-                <span class="ability-mod rollable" data-roll-type="duality" data-roll="1d12[hope]+1d12[fear]+@abilities.agility.mod" data-label="Agility">{system.abilities.agility.value}</span>
-            </div>
-            <div class="ability flexrow flex-group-center">
-                <label for="system.abilities.strength.value" class="resource-label rollable flexlarge align-left" data-roll-type="duality" data-roll="1d12[hope]+1d12[fear]+@abilities.strength.mod" data-label="Strength">Strength</label>
-                <input type="text" name="system.abilities.strength.value" value="{system.abilities.strength.value}" data-dtype="Number"/>
-                <span class="ability-mod rollable" data-roll-type="duality" data-roll="1d12[hope]+1d12[fear]+@abilities.strength.mod" data-label="Strength">{system.abilities.strength.value}</span>
-            </div>
-            <div class="ability flexrow flex-group-center">
-                <label for="system.abilities.finesse.value" class="resource-label rollable flexlarge align-left" data-roll-type="duality" data-roll="1d12[hope]+1d12[fear]+@abilities.finesse.mod" data-label="Finesse">Finesse</label>
-                <input type="text" name="system.abilities.finesse.value" value="{system.abilities.finesse.value}" data-dtype="Number"/>
-                <span class="ability-mod rollable" data-roll-type="duality" data-roll="1d12[hope]+1d12[fear]+@abilities.finesse.mod" data-label="Finesse">{system.abilities.finesse.value}</span>
-            </div>
-        </div>
-        <div class="abilities flex flex-row">
-            <div class="ability flexrow flex-group-center">
-                <label for="system.abilities.instinct.value" class="resource-label rollable flexlarge align-left" data-roll-type="duality" data-roll="1d12[hope]+1d12[fear]+@abilities.instinct.mod" data-label="Instinct">Instinct</label>
-                <input type="text" name="system.abilities.instinct.value" value="{system.abilities.instinct.value}" data-dtype="Number"/>
-                <span class="ability-mod rollable" data-roll-type="duality" data-roll="1d12[hope]+1d12[fear]+@abilities.instinct.mod" data-label="Instinct">{system.abilities.instinct.value}</span>
-            </div>
-            <div class="ability flexrow flex-group-center">
-                <label for="system.abilities.presence.value" class="resource-label rollable flexlarge align-left" data-roll-type="duality" data-roll="1d12[hope]+1d12[fear]+@abilities.presence.mod" data-label="Presence">Presence</label>
-                <input type="text" name="system.abilities.presence.value" value="{system.abilities.presence.value}" data-dtype="Number"/>
-                <span class="ability-mod rollable" data-roll-type="duality" data-roll="1d12[hope]+1d12[fear]+@abilities.presence.mod" data-label="Presence">{system.abilities.presence.value}</span>
-            </div>
-            <div class="ability flexrow flex-group-center">
-                <label for="system.abilities.knowledge.value" class="resource-label rollable flexlarge align-left" data-roll-type="duality" data-roll="1d12[hope]+1d12[fear]+@abilities.knowledge.mod" data-label="Knowledge">Knowledge</label>
-                <input type="text" name="system.abilities.knowledge.value" value="{system.abilities.knowledge.value}" data-dtype="Number"/>
-                <span class="ability-mod rollable" data-roll-type="duality" data-roll="1d12[hope]+1d12[fear]+@abilities.knowledge.mod" data-label="Knowledge">{system.abilities.knowledge.value}</span>
-            </div>
         </div>
     </div>
+
+<!--    <div class="flex flex-row">-->
+<!--        <div class="resources grid grid-3col">-->
+
+<!--            -->
+
+<!--            <div class="resource flex-group-center">-->
+
+<!--            </div>-->
+<!--        </div>-->
+<!--        <div class="abilities flex flex-row">-->
+<!--            <div class="ability flexrow flex-group-center">-->
+<!--                <label for="system.abilities.agility.value" class="resource-label rollable flexlarge align-left" data-roll-type="duality" data-roll="1d12[hope]+1d12[fear]+@abilities.agility.mod" data-label="Agility">Agility</label>-->
+<!--                <input type="text" name="system.abilities.agility.value" value="{system.abilities.agility.value}" data-dtype="Number"/>-->
+<!--                <span class="ability-mod rollable" data-roll-type="duality" data-roll="1d12[hope]+1d12[fear]+@abilities.agility.mod" data-label="Agility">{system.abilities.agility.value}</span>-->
+<!--            </div>-->
+<!--            <div class="ability flexrow flex-group-center">-->
+<!--                <label for="system.abilities.strength.value" class="resource-label rollable flexlarge align-left" data-roll-type="duality" data-roll="1d12[hope]+1d12[fear]+@abilities.strength.mod" data-label="Strength">Strength</label>-->
+<!--                <input type="text" name="system.abilities.strength.value" value="{system.abilities.strength.value}" data-dtype="Number"/>-->
+<!--                <span class="ability-mod rollable" data-roll-type="duality" data-roll="1d12[hope]+1d12[fear]+@abilities.strength.mod" data-label="Strength">{system.abilities.strength.value}</span>-->
+<!--            </div>-->
+<!--            <div class="ability flexrow flex-group-center">-->
+<!--                <label for="system.abilities.finesse.value" class="resource-label rollable flexlarge align-left" data-roll-type="duality" data-roll="1d12[hope]+1d12[fear]+@abilities.finesse.mod" data-label="Finesse">Finesse</label>-->
+<!--                <input type="text" name="system.abilities.finesse.value" value="{system.abilities.finesse.value}" data-dtype="Number"/>-->
+<!--                <span class="ability-mod rollable" data-roll-type="duality" data-roll="1d12[hope]+1d12[fear]+@abilities.finesse.mod" data-label="Finesse">{system.abilities.finesse.value}</span>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--        <div class="abilities flex flex-row">-->
+<!--            <div class="ability flexrow flex-group-center">-->
+<!--                <label for="system.abilities.instinct.value" class="resource-label rollable flexlarge align-left" data-roll-type="duality" data-roll="1d12[hope]+1d12[fear]+@abilities.instinct.mod" data-label="Instinct">Instinct</label>-->
+<!--                <input type="text" name="system.abilities.instinct.value" value="{system.abilities.instinct.value}" data-dtype="Number"/>-->
+<!--                <span class="ability-mod rollable" data-roll-type="duality" data-roll="1d12[hope]+1d12[fear]+@abilities.instinct.mod" data-label="Instinct">{system.abilities.instinct.value}</span>-->
+<!--            </div>-->
+<!--            <div class="ability flexrow flex-group-center">-->
+<!--                <label for="system.abilities.presence.value" class="resource-label rollable flexlarge align-left" data-roll-type="duality" data-roll="1d12[hope]+1d12[fear]+@abilities.presence.mod" data-label="Presence">Presence</label>-->
+<!--                <input type="text" name="system.abilities.presence.value" value="{system.abilities.presence.value}" data-dtype="Number"/>-->
+<!--                <span class="ability-mod rollable" data-roll-type="duality" data-roll="1d12[hope]+1d12[fear]+@abilities.presence.mod" data-label="Presence">{system.abilities.presence.value}</span>-->
+<!--            </div>-->
+<!--            <div class="ability flexrow flex-group-center">-->
+<!--                <label for="system.abilities.knowledge.value" class="resource-label rollable flexlarge align-left" data-roll-type="duality" data-roll="1d12[hope]+1d12[fear]+@abilities.knowledge.mod" data-label="Knowledge">Knowledge</label>-->
+<!--                <input type="text" name="system.abilities.knowledge.value" value="{system.abilities.knowledge.value}" data-dtype="Number"/>-->
+<!--                <span class="ability-mod rollable" data-roll-type="duality" data-roll="1d12[hope]+1d12[fear]+@abilities.knowledge.mod" data-label="Knowledge">{system.abilities.knowledge.value}</span>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--    </div>-->
 </header>
 
 <!-- Sheet Tab Navigation -->
